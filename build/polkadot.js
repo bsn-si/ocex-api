e58dd4ac0c44b6798831674a339133653201b999
Anton Shramko <antonshramko@yandex.ru>
2022-05-31 16:21:08 +0300

    rebuild

--- build/polkadot.js
+++ build/polkadot.js
@@ -2,8 +2,10 @@
 // Re-exports base polkadot packages for fix esm duplicate issue
 // https://github.com/polkadot-js/api/issues/4315
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.api = exports.util = exports.keyring = exports.utilCrypto = void 0;
+exports.api = exports.util = exports.keyring = exports.utilCrypto = exports.decodePair = void 0;
 const tslib_1 = require("tslib");
+const decode_1 = require("@polkadot/keyring/pair/decode");
+Object.defineProperty(exports, "decodePair", { enumerable: true, get: function () { return decode_1.decodePair; } });
 const utilCrypto = require("@polkadot/util-crypto");
 exports.utilCrypto = utilCrypto;
 const keyring = require("@polkadot/keyring");
